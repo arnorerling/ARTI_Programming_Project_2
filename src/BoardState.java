45d5404ba133ecb11d21859aa0e0f59772223c9e
sigurdurhelga <sigurdurhel15@ru.is>
2017-02-19 01:04:15 +0000

    changed functionality from arraylist to boolean[] for pawns

--- src/BoardState.java
+++ src/BoardState.java
@@ -1,131 +1,131 @@
 import java.util.List;
 import java.util.ArrayList;
 import java.lang.Math;
+import java.util.Arrays;
 
 public class BoardState {
 
 	public int height;
 	public int width;
-	public ArrayList<Point> whites;
+	public boolean[] whitesbool;
+	public boolean[] blacksbool;
+	public ArrayList<int[]> legalMoves;
+
 	public ArrayList<Point> blacks;
 	public boolean whitePlaying;
 
-	public BoardState(int _width, int _height, ArrayList<Point> _whites, ArrayList<Point> _blacks, boolean _whitePlaying ) {
+	public BoardState(int _width, int _height, boolean[] _whitesbool, boolean[] _blacksbool, boolean _whitePlaying ) {
 		this.width = _width;
 		this.height = _height;
-		this.whites = _whites;
-		this.blacks = _blacks;
+		this.whitesbool = _whitesbool;
+		this.blacksbool = _blacksbool;
 		this.whitePlaying = _whitePlaying;
+		this.legalMoves = this.legalMoves();
 		return;
 	}
 
-	public BoardState(BoardState _state) {
+	/*public BoardState(BoardState _state) {
 		this.width = _state.width;
 		this.height = _state.height;
-		this.whites = copyArrayList(whites);
-		this.blacks = copyArrayList(blacks);
-		this.whitePlaying = _state.whitePlaying;
-	}
+		//this.whites = copyArrayList(whites);
+		//this.blacks = copyArrayList(blacks);
 
-	private ArrayList<Point> copyArrayList(ArrayList<Point> cpy) {
-		ArrayList<Point> out = new ArrayList<Point>();
-		for(Point p : cpy) {
-			out.add(new Point(p.x, p.y));
-		}
-		return out;
-	}
+		this.whitePlaying = _state.whitePlaying;
+	}*/
 
 	public Boolean isGoal() {
-		for(Point p : whites) {
-			if(p.y == height) {
-				return true;
-			}
-		}
-		for(Point p : blacks) {
-			if(p.y == 1) {
+		for(int x = 0; x < this.width; x++) {
+			if(whitesbool[x+(this.width*(this.height-1))] || blacksbool[x]) {
 				return true;
 			}
 		}
 		if(legalMoves().size() == 0){
 			 return true;
 		}
+		
 		return false;
 	}
 
 	public ArrayList<int[]> legalMoves() {
 
 		ArrayList<int[]> legalMoves = new ArrayList<int[]>();
+		if(this.whitePlaying) {
+			for(int y = 0; y < this.height-1; y++) { // only go up to height - 2 to not calculate moves for whites on toprow
+				for(int x = 0; x < this.width; x++) {
+					if(whitesbool[x+(y*this.width)] == false) {
+						continue;
+					}
+					if(!blacksbool[x+((y+1)*this.width)] && !whitesbool[x+((y+1)*this.width)]) {
+						int[] move = new int[4];
+						move[0] = x+1;
+						move[1] = y+1;
+						move[2] = x+1;
+						move[3] = y+2;
+						legalMoves.add(move);
+					}
 
-		if(whitePlaying) {
+					if(x != this.width-1) {
+						if(blacksbool[(x+1)+((y+1) * this.width)]) {
+							int[] move = new int[4];
+							move[0] = x+1;
+							move[1] = y+1;
+							move[2] = x+2;
+							move[3] = y+2;
+							legalMoves.add(move);
+						}
+					}
 
-			for(Point pawn : whites) {
-
-				if(pawn.y == height) {
-					continue;
-				}
-
-				Point front = new Point(pawn.x, pawn.y+1);
-				Point diagonal_left = new Point(pawn.x-1, pawn.y+1);
-				Point diagonal_right = new Point(pawn.x+1, pawn.y+1);
-				if(!blacks.contains(front) && !whites.contains(front)) {
-					int[] move = new int[4];
-					move[0] = pawn.x;
-					move[1] = pawn.y;
-					move[2] = front.x;
-					move[3] = front.y;
-					legalMoves.add(move);
-				}
-				if(blacks.contains(diagonal_left)) {
-					int[] move = new int[4];
-					move[0] = pawn.x;
-					move[1] = pawn.y;
-					move[2] = diagonal_left.x;
-					move[3] = diagonal_left.y;
-					legalMoves.add(move);
-				}
-				if(blacks.contains(diagonal_right)) {
-					int[] move = new int[4];
-					move[0] = pawn.x;
-					move[1] = pawn.y;
-					move[2] = diagonal_right.x;
-					move[3] = diagonal_right.y;
-					legalMoves.add(move);
+					if(x!= 0) {
+						if(blacksbool[(x-1)+((y+1) * this.width)]) {
+							int[] move = new int[4];
+							move[0] = x+1;
+							move[1] = y+1;
+							move[2] = x;
+							move[3] = y+2;
+							legalMoves.add(move);
+						}
+					}
 				}
 			}
+
 		}
+
 		else{
-			for(Point pawn : blacks) {
+			for(int y = 1; y < this.height; y++) { // start at 1 to not calculate moves for blacks in the bottom row
+				for(int x = 0; x < this.width; x++) {
+					if(blacksbool[x+(y*this.width)] == false) {
+						continue;
+					}
+					if(!blacksbool[x+((y-1)*this.width)] && !whitesbool[x+((y-1)*this.width)]) {
+						int[] move = new int[4];
+						move[0] = x+1;
+						move[1] = y+1;
+						move[2] = x+1;
+						move[3] = y;
+						legalMoves.add(move);
+					}
+					if(x != this.width-1) {
+						if(whitesbool[(x+1)+((y-1) * this.width)]) {
+							int[] move = new int[4];
+							move[0] = x+1;
+							move[1] = y+1;
+							move[2] = x+2;
+							move[3] = y;
+							legalMoves.add(move);
+						}
+					}
+					if(x != 0) {
+						if(whitesbool[(x-1)+((y-1) * this.width)]) {
+							int[] move = new int[4];
+							move[0] = x+1;
+							move[1] = y+1;
+							move[2] = x;
+							move[3] = y;
+							legalMoves.add(move);
+						}
+					}
+					
 
-				if(pawn.y == 1) {
-					continue;
-				}
-
-				Point front = new Point(pawn.x, pawn.y-1);
-				Point diagonal_left = new Point(pawn.x-1, pawn.y-1);
-				Point diagonal_right = new Point(pawn.x+1, pawn.y-1);
-				if(!blacks.contains(front) && !whites.contains(front)) {
-					int[] move = new int[4];
-					move[0] = pawn.x;
-					move[1] = pawn.y;
-					move[2] = front.x;
-					move[3] = front.y;
-					legalMoves.add(move);
-				}
-				if(whites.contains(diagonal_left)) {
-					int[] move = new int[4];
-					move[0] = pawn.x;
-					move[1] = pawn.y;
-					move[2] = diagonal_left.x;
-					move[3] = diagonal_left.y;
-					legalMoves.add(move);
-				}
-				if(whites.contains(diagonal_right)) {
-					int[] move = new int[4];
-					move[0] = pawn.x;
-					move[1] = pawn.y;
-					move[2] = diagonal_right.x;
-					move[3] = diagonal_right.y;
-					legalMoves.add(move);
 				}
 			}
 		}
@@ -133,42 +133,79 @@ public class BoardState {
 	}
 
 	public BoardState executeMove(int[] move) {
-		Point pawn_original = new Point(move[0], move[1]);
-		Point pawn_move_to = new Point(move[2], move[3]);
-		ArrayList<Point> newWhites = copyArrayList(whites);
-		ArrayList<Point> newBlacks = copyArrayList(blacks);
+		boolean[] newWhitesBool = Arrays.copyOf(this.whitesbool, this.whitesbool.length);
+		boolean[] newBlacksBool = Arrays.copyOf(this.blacksbool, this.blacksbool.length);
+
+		int posFrom = (move[0]-1) + ((move[1]-1) * this.width);
+		int posTo = (move[2]-1) + ((move[3]-1) * this.width);
 
 		if(whitePlaying) {
 			//System.out.println("blacks before possible remove " + newBlacks.size());
-			if(newBlacks.contains(pawn_move_to)) {
+			if(newBlacksBool[posTo] == true) {
 				//System.out.println("black removal");
-				newBlacks.remove(pawn_move_to);
+				newBlacksBool[posTo] = false;
 			}
 			//System.out.println("blacks after possible remove " + newBlacks.size());
-			newWhites.add(pawn_move_to);
-			newWhites.remove(pawn_original);
+			newWhitesBool[posTo] = true;
+			newWhitesBool[posFrom] = false;
 		}
 		else{
-			if(newWhites.contains(pawn_move_to)) {
-				newWhites.remove(pawn_move_to);
+			if(newWhitesBool[posTo] == true) {
+				newWhitesBool[posTo] = false;
 			}
-			newBlacks.add(pawn_move_to);
-			newBlacks.remove(pawn_original);
+			newBlacksBool[posTo] = true;
+			newBlacksBool[posFrom] = false;
 		}
-		return new BoardState(width, height, newWhites, newBlacks, !whitePlaying);
+		return new BoardState(width, height, newWhitesBool, newBlacksBool, !whitePlaying);
 	}
 
 
 	public int evaluate(){
-		int distOfMostAdvancedWhite = Integer.MAX_VALUE;
-		int distOfMostAdvancedBlack = Integer.MAX_VALUE;
+		int distOfMostAdvancedWhite = this.height;
+		int distOfMostAdvancedBlack = 0;
+		int onWinState = 0;
 
-		for(Point pawn : whites) {
-			distOfMostAdvancedWhite = Math.min(distOfMostAdvancedWhite, height - pawn.y);
-		}
+		boolean foundPawn = false;
 
-		for(Point pawn : blacks) {
-			distOfMostAdvancedBlack = Math.min(distOfMostAdvancedBlack, pawn.y-1);
+		for(int y = this.height-1; y > 0; y--) { // 
+
+				for(int x = 0; x < this.width; x++) {
+
+					if(this.whitesbool[x+(y*this.width)] == true) {
+
+						if(y == this.height-1) {
+							//onWinState = 100;
+						}
+
+						distOfMostAdvancedWhite = this.height - y - 1;
+						foundPawn = true;
+						break;
+
+					}
+				}
+
+				if(foundPawn) {
+					break;
+				}
+
+			}
+
+		foundPawn = false;
+
+		for(int y = 0; y < this.height-2; y++) {
+			for(int x = 0; x < this.width; x++) {
+				if(this.blacksbool[x+(y*this.width)] == true) {
+					if(y == 0) {
+						//onWinState = 100;
+					}
+					distOfMostAdvancedBlack = y;
+					foundPawn = true;
+					break;
+				}
+			}
+			if(foundPawn) {
+				break;
+			}
 		}
 
 		// System.out.println("size of whites is " + whites.size());
@@ -176,7 +213,7 @@ public class BoardState {
 		// System.out.println("distOfMostAdvancedWhite is " + distOfMostAdvancedWhite);
 		// System.out.println("distOfMostAdvancedBlack is " + distOfMostAdvancedBlack);
 
-		return 50 - distOfMostAdvancedWhite + distOfMostAdvancedBlack;
+		return 50 - distOfMostAdvancedWhite + distOfMostAdvancedBlack + onWinState;
 	}
 
 
@@ -184,29 +221,27 @@ public class BoardState {
 	public boolean equals(Object obj){
 
 		BoardState other = (BoardState) obj;
-		if(this.whites.size() != other.whites.size()) {
+		if(this.whitesbool.length != other.whitesbool.length) {
 			return false;
 		}
-		if(this.blacks.size() != other.blacks.size()) {
+		if(this.blacksbool.length != other.blacksbool.length) {
 			return false;
 		}
-		for(int i = 0; i < whites.size(); i++) {
-			if(!this.whites.get(i).equals(other.whites.get(i))) {
+		for(int i = 0; i < whitesbool.length; i++) {
+			if(!(this.whitesbool[i] == other.whitesbool[i])) {
 				return false;
 			}
 		}
-		for(int i = 0; i < blacks.size(); i++) {
-			if(!this.blacks.get(i).equals(other.blacks.get(i))) {
+		for(int i = 0; i < blacksbool.length; i++) {
+			if(!(this.blacksbool[i] == other.blacksbool[i])) {
 				return false;
+
 			}
-		}
-		if(width != other.width || height != other.height){
-			return false;
 		}
 		return true;
 	}
 
 	public String toString() {
-		return "whitePlaying " + Boolean.toString(whitePlaying);
+		return "whitePlaying";
 	}
 }
